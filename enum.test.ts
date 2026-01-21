0f45690197c7923c61f79dcd0d246a7186913d3f
Laurent Turpin <laurent.turpin@senova.fr>
2026-01-21 09:46:06 +0100

    misc

--- enum.test.ts
+++ enum.test.ts
@@ -10,4 +10,14 @@ describe('Enums', () => {
         expect(Color[1]).toBe('Green');
         expect(Color[Color.Red]).toBe('Red');
     });
+
+    it('should returns the enum key as a string', () => {
+        enum LABELS {
+            MAX = 'MAX',
+            ESTIMATED = 'ESTIMATED',
+        }
+
+        expect(LABELS['MAX']).toBe('MAX');
+        expect(LABELS[LABELS.ESTIMATED]).toBe('ESTIMATED');
+    });
 });
--- fetch.test.ts
+++ fetch.test.ts
@@ -1,7 +1,15 @@
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
-import { beforeEach } from 'node:test';
-import { afterAll, afterEach, beforeAll, expect, it, vi } from 'vitest';
+import {
+    afterAll,
+    afterEach,
+    beforeAll,
+    beforeEach,
+    describe,
+    expect,
+    it,
+    vi,
+} from 'vitest';
 
 const spy = vi.fn();
 
@@ -35,38 +43,40 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 beforeEach(() => spy.mockClear());
 
-it('should manage to get an arrayBuffer', async () => {
-    const res = await fetch('https://example.com/resource');
-    const buffer = await res.arrayBuffer();
+describe('fetch', () => {
+    it('should manage to get an arrayBuffer', async () => {
+        const res = await fetch('https://example.com/resource');
+        const buffer = await res.arrayBuffer();
 
-    expect(spy).toHaveBeenCalledWith('resource');
+        expect(spy).toHaveBeenCalledWith('resource');
 
-    const uint8Array = new Uint8Array(buffer);
+        const uint8Array = new Uint8Array(buffer);
 
-    expect(uint8Array).toHaveLength(3);
-    expect(uint8Array).toContain(1);
-    expect(uint8Array).toContain(2);
-    expect(uint8Array).toContain(3);
-});
+        expect(uint8Array).toHaveLength(3);
+        expect(uint8Array).toContain(1);
+        expect(uint8Array).toContain(2);
+        expect(uint8Array).toContain(3);
+    });
 
-it('should manage to turn the response into an array buffer, even though the response is a 404 (Response still have a body)', async () => {
-    const res = await fetch('https://example.com/notFound');
-    const buffer = await res.arrayBuffer();
+    it('should manage to turn the response into an array buffer, even though the response is a 404 (Response still have a body)', async () => {
+        const res = await fetch('https://example.com/notFound');
+        const buffer = await res.arrayBuffer();
 
-    const uint8Array = new Uint8Array(buffer);
-    expect(res.ok).toBeFalsy();
-    expect(res.status).toBe(404);
+        const uint8Array = new Uint8Array(buffer);
+        expect(res.ok).toBeFalsy();
+        expect(res.status).toBe(404);
 
-    expect(uint8Array).toBeDefined();
-});
+        expect(uint8Array).toBeDefined();
+    });
 
-it('should response with ok even if the request is redirected (300)', async () => {
-    const res = await fetch('https://example.com/redirect');
+    it('should response with ok even if the request is redirected (300)', async () => {
+        const res = await fetch('https://example.com/redirect');
 
-    expect(spy).toHaveBeenNthCalledWith(1, 'redirect');
-    expect(spy).toHaveBeenNthCalledWith(2, 'resource');
-    expect(res.ok).toBeTruthy();
+        expect(spy).toHaveBeenNthCalledWith(1, 'redirect');
+        expect(spy).toHaveBeenNthCalledWith(2, 'resource');
+        expect(res.ok).toBeTruthy();
 
-    // Why redirected is not set to true ??
-    expect(res.redirected).not.toBeTruthy();
+        // Why redirected is not set to true ??
+        expect(res.redirected).not.toBeTruthy();
+    });
 });
